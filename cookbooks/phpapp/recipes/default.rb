57492a224b4b1939f429dd0edff32dc1d575dd4e
root <root@precise64.(none)>
2017-05-19 09:22:25 +0000

    clean receipe for apache

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -8,13 +8,6 @@
 #
 include_recipe "apache2"
 
-mysql_service 'foo' do
-  port '3306'
-  version '5.5'
-  initial_root_password 'changeme'
-  action [:create, :start]
-end
-
 apache_site "default" do
   enable true
 end
